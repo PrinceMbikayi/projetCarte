d3c2d741efa57407560d82a287ec97b73974d0de
PrinceMbikayi <pmbikayi01@gmail.com>
2023-12-14 09:33:10 +0100

    API Search

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const forms= document.querySelector(".containerr .form"),
+      pwShowHide = document.querySelectorAll(".eye-icon"),
+      links = document.querySelectorAll(".link");
+
+pwShowHide.forEach(eyeIcon => {
+    eyeIcon.addEventListener("click", () => {
+        let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
+
+        pwFields.forEach(password => {
+            if(password.type === "password"){
+                password.type = "text";
+                eyeIcon.classList.replace("bx-hide", "bx-show");
+                return;
+            }
+            password.type = "password";
+            eyeIcon.classList.replace("bx-show", "bx-hide");
+        })
+    })
+})
+
+links.forEach(link => {
+    link.addEventListener("click", e => {
+        e.preventDefault(); // revenir en arriere du formulaire
+        form.classList.toggle("show-signup");
+    })
+})
